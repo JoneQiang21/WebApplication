c70dbbf7cd648cd2bf2c9e4f65e341e45514a17e
sunshuo1998 <119727292+sunshuo1998@users.noreply.github.com>
2022-12-17 16:13:19 +0800

    Add files via upload

--- product.sql
+++ product.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80012 (8.0.12)
  File Encoding         : 65001
 
- Date: 05/12/2022 20:21:25
+ Date: 17/12/2022 16:12:50
 */
 
 SET NAMES utf8mb4;
@@ -122,50 +122,66 @@ CREATE TABLE `products`  (
   `size` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `intro` tinytext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `discount` int(10) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
-INSERT INTO `products` VALUES (1, '3.jpg', 'ambi-Strawberry', '1', 258.03, 'accessory', 'watch', 'business', '2022-09-14', 'Standard/', 'yellow', 'This is some intro');
-INSERT INTO `products` VALUES (30, '4.jpg', 'omni-Cherry', '1', 229.09, 'shirt', 'top', 'leisure', '2022-04-01', 'FS/S/M/L/XL', 'red', 'This is some intro');
-INSERT INTO `products` VALUES (3, '5.jpg', 'Kiwi', '1', 252.4, 'accessory', 'belt', 'decoration', '2022-10-12', 'Standard/', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (4, '7.jpg', 'omni-vrape', '1', 82.75, 'tshirt', 'top', 'sport', '2022-09-06', 'FS/S/M/L/XL', 'white', 'This is some intro');
-INSERT INTO `products` VALUES (5, '9.jpg', 'Raipberry', '1', 149.46, 'shirt', 'top', 'fashion', '2022-11-16', 'FS/S/M/L/XL', 'pink', 'This is some intro');
-INSERT INTO `products` VALUES (6, '10.jpg', 'iRambutan', '1', 471.25, 'shirt', 'top', 'fashion', '2022-09-13', 'FS/S/M/L/XL', 'white', 'This is some intro');
-INSERT INTO `products` VALUES (21, '11.jpg', 'Pluots', '1', 188.22, 'shirt', 'top', 'business', '2022-09-30', 'FS/S/M/L/XL', 'grey', 'This is some intro');
-INSERT INTO `products` VALUES (8, '12.jpg', 'Caerry', '1', 291.78, 'bag', 'bag', 'sport', '2022-09-24', 'Standard/', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (9, '13.jpg', 'Kiqi', '1', 46.88, 'bag', 'bag', 'sport', '2022-11-01', 'Standard/', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (7, 'pic2.jpg', 'Orange core', '1', 9.25, 'tshirt', 'top', 'leisure', '2022-11-20', 'FS/S/M/L/XL', 'blue', 'This is some intro');
-INSERT INTO `products` VALUES (12, 'pic4.jpg', 'Rambutan', '1', 306.19, 'tshirt', 'top', 'leisure', '2022-03-13', 'FS/S/M/L/XL', 'ocean', 'This is some intro');
-INSERT INTO `products` VALUES (13, 'pic5.jpg', 'ambi-Grjpe', '1', 225.37, 'shirt', 'top', 'leisure', '2022-11-22', 'FS/S/M/L/XL', 'purple', 'This is some intro');
-INSERT INTO `products` VALUES (2, '8.jpg', 'Myngo core', '0', 301.05, 'shirt', 'top', 'leisure', '2022-07-05', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (15, 'pic1.jpg', 'Stravberry', '0', 384.88, 'tshirt', 'top', 'leisure', '2022-09-18', 'FS/S/M/L/XL', 'white', 'This is some intro');
-INSERT INTO `products` VALUES (16, 'pic3.jpg', 'Rasbberry core', '0', 5.29, 'dress', 'top', 'fashion', '2022-06-17', 'FS/S/M/L/XL', 'green', 'This is some intro');
-INSERT INTO `products` VALUES (17, 'pic6.jpg', 'omni-Apple', '0', 57.8, 'loungewear', 'top', 'household', '2022-05-22', 'FS/S/M/L/XL', 'red', 'This is some intro');
-INSERT INTO `products` VALUES (18, 's4.jpg', 'Kswi', '0', 293.87, 'dress', 'top', 'fashion', '2022-06-18', 'FS/S/M/L/XL', 'red', 'This is some intro');
-INSERT INTO `products` VALUES (19, '16.jpg', 'Mango pro', '0', 263.17, 'bag', 'bag', 'fashion', '2022-05-28', 'Standard/', 'brown', 'This is some intro');
-INSERT INTO `products` VALUES (22, '17.png', 'Apple core', '0', 171.17, 'jacket', 'top', 'fashion', '2021-08-11', 'FS/S/M/L/XL', 'grey', 'This is some intro');
-INSERT INTO `products` VALUES (23, '18.png', 'Pluots core', '0', 226.48, 'shoes', 'shoes', 'fashion', '2022-05-28', 'S/M/L', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (24, '19.png', 'Cherny core', '1', 463.74, 'jacket', 'top', 'leisure', '2021-12-10', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (25, '20.png', 'Raspberry premium', '0', 317.59, 'accessory', 'scarf', 'decoration', '2021-11-20', 'Standard/', 'pink', 'This is some intro');
-INSERT INTO `products` VALUES (26, '21.png', 'ypple air', '1', 483.64, 'suit', 'top', 'business', '2021-09-28', 'S/M/L', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (27, '22.jpg', 'Grape', '1', 482.28, 'hoodie', 'top', 'sport', '2022-06-27', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (28, '23.png', 'Cheroy', '0', 189.35, 'accessory', 'watch', 'decoration', '2020-01-30', 'Standard/', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (29, '24.jpg', 'Orxnge premium', '1', 233.33, 'jeans', 'bottom', 'leisure', '2022-02-10', 'FS/S/M/L/XL', 'blue', 'This is some intro');
-INSERT INTO `products` VALUES (14, '25.jpg', 'iAppwe', '1', 490.01, 'jeans', 'bottom', 'leisure', '2022-05-15', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (31, '26.jpg', 'Grype', '1', 162.28, 'swimwear', 'bottom', 'fashion', '2022-02-26', 'FS/S/M/L/XL', 'cyan', 'This is some intro');
-INSERT INTO `products` VALUES (32, '27.jpg', 'Styawberry core', '1', 0.45, 'swimwear', 'bottom', 'sport', '2020-06-06', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (33, '28.jpg', 'ambi-Cherry', '1', 50.07, 'swimwear', 'bottom', 'sport', '2020-06-29', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (34, '29.jpg', 'omni-Appae', '1', 383.19, 'trousers', 'bottom', 'sport', '2022-11-07', 'FS/S/M/L/XL', 'black', 'This is some intro');
-INSERT INTO `products` VALUES (35, '30.jpg', 'Orxnge premium', '0', 0.45, 'shoes', 'shoes', 'fashion', '2020-05-18', 'FS/S/M/L/XL', 'white', 'This is some intro');
-INSERT INTO `products` VALUES (36, '31.jpg', 'iAppwe', '0', 50.07, 'shoes', 'shoes', 'fashion', '2021-10-29', 'FS/S/M/L/XL', 'pink', 'This is some intro');
-INSERT INTO `products` VALUES (37, '32.jpg', 'Grype', '0', 383.19, 'shoes', 'shoes', 'fashion', '2021-02-09', 'FS/S/M/L/XL', 'green', 'This is some intro');
-INSERT INTO `products` VALUES (38, '33.jpg', 'Styawberry core', '0', 18.09, 'accessory', 'jewellery', 'decoration', '2022-08-22', 'Standard/', 'yellow', 'This is some intro');
-INSERT INTO `products` VALUES (39, '34.jpg', 'ambi-Cherry', '0', 359, 'bag', 'bag', 'fashion', '2021-08-03', 'Standard/', 'white', 'This is some intro');
-INSERT INTO `products` VALUES (40, '35.jpg', 'ultra-lrange', '0', 235.19, 'hat', 'hat', 'fashion', '2020-02-08', 'Standard/', 'red', 'This is some intro');
-INSERT INTO `products` VALUES (41, '36.jpg', 'Ramiutan', '0', 10.11, 'hat', 'hat', 'leisure', '2022-05-03', 'Standard/', 'orange', 'This is some intro');
-INSERT INTO `products` VALUES (42, '37.jpg', 'Kiwi', '0', 212.76, 'loungewear', 'top', 'household', '2020-03-31', 'FS/S/M/L/XL', 'olive', 'This is some intro');
+INSERT INTO `products` VALUES (1, '3.jpg', 'ambi-Strawberry', '1', 258.03, 'accessory', 'watch', 'business', '2022-09-14', 'Standard/', 'yellow', 'This is some intro', 0);
+INSERT INTO `products` VALUES (30, '4.jpg', 'omni-Cherry', '1', 229.09, 'shirt', 'top', 'leisure', '2022-04-01', 'FS/S/M/L/XL', 'red', 'This is some intro', 0);
+INSERT INTO `products` VALUES (3, '5.jpg', 'Kiwi', '1', 252.4, 'accessory', 'belt', 'decoration', '2022-10-12', 'Standard/', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (4, '7.jpg', 'omni-vrape', '1', 82.75, 'tshirt', 'top', 'sport', '2022-09-06', 'FS/S/M/L/XL', 'white', 'This is some intro', 0);
+INSERT INTO `products` VALUES (5, '9.jpg', 'Raipberry', '1', 149.46, 'shirt', 'top', 'fashion', '2022-11-16', 'FS/S/M/L/XL', 'pink', 'This is some intro', 0);
+INSERT INTO `products` VALUES (6, '10.jpg', 'iRambutan', '1', 471.25, 'shirt', 'top', 'fashion', '2022-09-13', 'FS/S/M/L/XL', 'white', 'This is some intro', 0);
+INSERT INTO `products` VALUES (21, '11.jpg', 'Pluots', '1', 188.22, 'shirt', 'top', 'business', '2022-09-30', 'FS/S/M/L/XL', 'grey', 'This is some intro', 0);
+INSERT INTO `products` VALUES (8, '12.jpg', 'Caerry', '1', 291.78, 'bag', 'bag', 'sport', '2022-09-24', 'Standard/', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (9, '13.jpg', 'Kiqi', '1', 46.88, 'bag', 'bag', 'sport', '2022-11-01', 'Standard/', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (7, 'pic2.jpg', 'Orange core', '1', 9.25, 'tshirt', 'top', 'leisure', '2022-11-20', 'FS/S/M/L/XL', 'blue', 'This is some intro', 1);
+INSERT INTO `products` VALUES (12, 'pic4.jpg', 'Rambutan', '1', 306.19, 'tshirt', 'top', 'leisure', '2022-03-13', 'FS/S/M/L/XL', 'ocean', 'This is some intro', 0);
+INSERT INTO `products` VALUES (13, 'pic5.jpg', 'ambi-Grjpe', '1', 225.37, 'shirt', 'top', 'leisure', '2022-11-22', 'FS/S/M/L/XL', 'purple', 'This is some intro', 1);
+INSERT INTO `products` VALUES (2, '8.jpg', 'Myngo core', '0', 301.05, 'shirt', 'top', 'leisure', '2022-07-05', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (15, 'pic1.jpg', 'Stravberry', '0', 384.88, 'tshirt', 'top', 'leisure', '2022-09-18', 'FS/S/M/L/XL', 'white', 'This is some intro', 0);
+INSERT INTO `products` VALUES (16, 'pic3.jpg', 'Rasbberry core', '0', 5.29, 'dress', 'top', 'fashion', '2022-06-17', 'FS/S/M/L/XL', 'green', 'This is some intro', 0);
+INSERT INTO `products` VALUES (17, 'pic6.jpg', 'omni-Apple', '0', 57.8, 'loungewear', 'top', 'household', '2022-05-22', 'FS/S/M/L/XL', 'red', 'This is some intro', 0);
+INSERT INTO `products` VALUES (18, 's4.jpg', 'Kswi', '0', 293.87, 'dress', 'top', 'fashion', '2022-06-18', 'FS/S/M/L/XL', 'red', 'This is some intro', 1);
+INSERT INTO `products` VALUES (19, '16.jpg', 'Mango pro', '0', 263.17, 'bag', 'bag', 'fashion', '2022-05-28', 'Standard/', 'brown', 'This is some intro', 0);
+INSERT INTO `products` VALUES (22, '17.png', 'Apple core', '0', 171.17, 'jacket', 'top', 'fashion', '2021-08-11', 'FS/S/M/L/XL', 'grey', 'This is some intro', 0);
+INSERT INTO `products` VALUES (23, '18.png', 'Pluots core', '0', 226.48, 'shoes', 'shoes', 'fashion', '2022-05-28', 'S/M/L', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (24, '19.png', 'Cherny core', '1', 463.74, 'jacket', 'top', 'leisure', '2021-12-10', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (25, '20.png', 'Raspberry premium', '0', 317.59, 'accessory', 'scarf', 'decoration', '2021-11-20', 'Standard/', 'pink', 'This is some intro', 0);
+INSERT INTO `products` VALUES (26, '21.png', 'ypple air', '1', 483.64, 'suit', 'top', 'business', '2021-09-28', 'S/M/L', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (27, '22.jpg', 'Grape', '1', 482.28, 'hoodie', 'top', 'sport', '2022-06-27', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (28, '23.png', 'Cheroy', '0', 189.35, 'accessory', 'watch', 'decoration', '2020-01-30', 'Standard/', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (29, '24.jpg', 'Orxnge premium', '1', 233.33, 'jeans', 'bottom', 'leisure', '2022-02-10', 'FS/S/M/L/XL', 'blue', 'This is some intro', 0);
+INSERT INTO `products` VALUES (14, '25.jpg', 'iAppwe', '1', 490.01, 'jeans', 'bottom', 'leisure', '2022-05-15', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (31, '26.jpg', 'Grype', '1', 162.28, 'swimwear', 'bottom', 'fashion', '2022-02-26', 'FS/S/M/L/XL', 'cyan', 'This is some intro', 1);
+INSERT INTO `products` VALUES (32, '27.jpg', 'Styawberry core', '1', 0.45, 'swimwear', 'bottom', 'sport', '2020-06-06', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (33, '28.jpg', 'ambi-Cherry', '1', 50.07, 'swimwear', 'top', 'sport', '2020-06-29', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (34, '29.jpg', 'omni-Appae', '1', 383.19, 'trousers', 'bottom', 'sport', '2022-11-07', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (35, '30.jpg', 'Orxnge premium', '0', 24, 'shoes', 'shoes', 'fashion', '2020-05-18', 'FS/S/M/L/XL', 'white', 'This is some intro', 0);
+INSERT INTO `products` VALUES (36, '31.jpg', 'iAppwe', '0', 50.07, 'shoes', 'shoes', 'fashion', '2021-10-29', 'FS/S/M/L/XL', 'pink', 'This is some intro', 0);
+INSERT INTO `products` VALUES (37, '32.jpg', 'Grype', '0', 383.19, 'shoes', 'shoes', 'fashion', '2021-02-09', 'FS/S/M/L/XL', 'green', 'This is some intro', 0);
+INSERT INTO `products` VALUES (38, '33.jpg', 'Styawberry core', '0', 18.09, 'accessory', 'jewellery', 'decoration', '2022-08-22', 'Standard/', 'yellow', 'This is some intro', 0);
+INSERT INTO `products` VALUES (39, '34.jpg', 'ambi-Cherry', '0', 359, 'bag', 'bag', 'fashion', '2021-08-03', 'Standard/', 'white', 'This is some intro', 0);
+INSERT INTO `products` VALUES (40, '35.jpg', 'ultra-lrange', '0', 235.19, 'hat', 'hat', 'fashion', '2020-02-08', 'Standard/', 'red', 'This is some intro', 0);
+INSERT INTO `products` VALUES (41, '36.jpg', 'Ramiutan', '0', 10.11, 'hat', 'hat', 'leisure', '2022-05-03', 'Standard/', 'orange', 'This is some intro', 0);
+INSERT INTO `products` VALUES (42, '37.jpg', 'Kiwi', '0', 212.76, 'loungewear', 'top', 'household', '2020-03-31', 'FS/S/M/L/XL', 'olive', 'This is some intro', 0);
+INSERT INTO `products` VALUES (43, '38.jpg', 'ytrawberry', '0', 340.51, 'shoes', 'shoes', 'fashion', '2020-07-25', 'S/M/L', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (44, '39.jpg', 'Strawperry', '0', 130.79, 'skirt', 'bottom', 'leisure', '2021-11-08', 'FS/S/M/L/XL', 'pink', 'This is some intro', 0);
+INSERT INTO `products` VALUES (45, '40.jpg', 'iStrawberry', '0', 372.35, 'skirt', 'bottom', 'fashion', '2021-08-17', 'FS/S/M/L/XL', 'grey', 'This is some intro', 0);
+INSERT INTO `products` VALUES (46, '41.jpg', 'Rambutan mini', '0', 199.79, 'skirt', 'bottom', 'leisure', '2021-10-25', 'FS/S/M/L/XL', 'green', 'This is some intro', 0);
+INSERT INTO `products` VALUES (47, '42.jpg', 'Kiwi mini', '0', 208.46, 'jacket', 'top', 'fashion', '2022-11-26', 'FS/S/M/L/XL', 'brown', 'This is some intro', 0);
+INSERT INTO `products` VALUES (48, '43.jpg', 'Apple', '0', 485.35, 'suit', 'top', 'business', '2021-01-30', 'FS/S/M/L/XL', 'green', 'This is some intro', 0);
+INSERT INTO `products` VALUES (49, '44.jpg', 'ambi-Orange', '0', 100.19, 'sunglasses', 'glasses', 'decoration', '2022-05-26', 'Standard/', 'orange', 'This is some intro', 0);
+INSERT INTO `products` VALUES (50, '45.jpg', 'ambi-Rambutan', '0', 255.98, 'swimwear', 'top', 'leisure', '2022-02-10', 'FS/S/M/L/XL', 'yellow', 'This is some intro', 1);
+INSERT INTO `products` VALUES (51, '46.jpg', 'Strawnerry pi', '0', 360.6, 'swimwear', 'top', 'sport', '2022-05-15', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (52, '47.jpg', 'omni-Appae', '0', 342.49, 'hoodie', 'top', 'leisure', '2022-02-26', 'FS/S/M/L/XL', 'red', 'This is some intro', 0);
+INSERT INTO `products` VALUES (53, '48.jpg', 'eluots elite', '0', 233.33, 'hoodie', 'top', 'leisure', '2020-06-06', 'FS/S/M/L/XL', 'green', 'This is some intro', 0);
+INSERT INTO `products` VALUES (54, '49.jpg', 'Rambutan premium', '0', 429.84, 'jeans', 'bottom', 'leisure', '2021-06-14', 'FS/S/M/L/XL', 'blue', 'This is some intro', 0);
+INSERT INTO `products` VALUES (55, '50.jpg', 'xGrape', '0', 24.39, 'jeans', 'bottom', 'leisure', '2022-11-23', 'FS/S/M/L/XL', 'black', 'This is some intro', 0);
+INSERT INTO `products` VALUES (56, '51.jpg', 'ytrawberry', '0', 340.51, 'trousers', 'bottom', 'fashion', '2020-07-25', 'FS/S/M/L/XL', 'grey', 'This is some intro', 0);
+INSERT INTO `products` VALUES (57, '52.jpg', 'Strawperry', '0', 130.79, 'trousers', 'bottom', 'fashion', '2021-11-08', 'FS/S/M/L/XL', 'white', 'This is some intro', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
